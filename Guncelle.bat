6a9be68b142463e04d505b99994990e7d2755131
Volkan <volkan@example.com>
2026-04-22 15:53:33 +0300

    Auto Update: Yeni ozellikler

--- Guncelle.bat
+++ Guncelle.bat
@@ -5,42 +5,27 @@ echo ----------------------------------------------------
 echo BUTCEM AI - YZ OTOMATIK GITHUB BOTU
 echo ----------------------------------------------------
 echo.
+echo [ISLEM] Git ayarlari yapilandiriliyor...
 
-if exist ".git" goto :HasGit
-
-echo [BILGI] Bu klasor henuz GitHub sistemine dogrudan bagli degil. 
-echo Otomatik botun calisabilmesi icin bir kereye mahsus 
-echo GitHub deponuzun linkini girmeniz gerekiyor.
-echo Ornek link: https://github.com/kullanici-adiniz/butcem.git
-echo.
-set /p REPO_URL="Linkinizi buraya yapistirin ve Enter tusuna basin: "
-
-if "!REPO_URL!"=="" (
-    echo [HATA] Link girmediginiz icin islem iptal edildi.
-    pause
-    exit /b
+if not exist ".git" (
+    git init
+    git branch -M main
 )
 
-echo.
-echo [ISLEM] Git altyapisi kuruluyor...
-git init
-git branch -M main
-git remote add origin !REPO_URL!
-echo [BASARILI] Klasor GitHub sistemine baglandi!
-echo.
+:: Yanlis girilmis olma ihtimaline karsi eski linki silip dogrusunu ekliyoruz
+git remote remove origin 2>nul
+git remote add origin https://github.com/volkan356/butcem.git
 
-:HasGit
 echo [BILGI] Guncellemeler Github veritabanina gonderiliyor, lutfen bekleyin...
 git add .
-git commit -m "Auto Update: Yeni ozellikler ve hatasiz bot"
+git commit -m "Auto Update: Yeni ozellikler"
 git push -u origin main
 
 echo.
 if %ERRORLEVEL% EQU 0 (
     echo [BASARILI] Tum degisiklikler telefonunuza aktarildi. Sayfanizi 1 dakika icinde yenileyebilirsiniz.
 ) else (
-    echo [UYARI] Bir hata olustu. Github linki hatali veya internetiniz kesik olabilir. 
-    echo Lutfen ekrandaki hatayi okuyun.
+    echo [UYARI] Bir hata olustu. Internette kesinti olabilir veya Github sifreniz istenebilir.
 )
 echo.
 pause
